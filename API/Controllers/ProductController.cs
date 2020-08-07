5b8275a6eb4a2114c2c18129502a22d300fba875
isidoreduc <isidoreducasse79@gmail.com>
2020-08-07 11:58:20 +0200

    product model and controller

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public string GetAll() => "all products";
+
+        [HttpGet("{id}")]
+        public string GetOne(int id) => "one product";        
+    }
+}
