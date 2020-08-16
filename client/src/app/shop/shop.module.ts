e01693ff168a94a8a464b68688184b5f46025273
isidoreduc <isidoreducasse79@gmail.com>
2020-08-16 14:43:48 +0300

    adding ngx-bootstrap pagination

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ShopService } from './shop.service';
 import { ProductItemComponent } from './product-item/product-item.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ShopComponent, ProductItemComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [ShopComponent],
 })
 export class ShopModule {}
