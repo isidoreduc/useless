f911847a5c486d5fa42949d0a013fbf3e3a0b13b
isidoreduc <isidoreducasse79@gmail.com>
2020-09-09 10:46:10 +0200

    display basket count in navbar and setup the basket component template

--- client/src/app/core/navbar/navbar.component.ts
+++ client/src/app/core/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from 'src/app/basket/basket.service';
+import { Observable } from 'rxjs';
+import { IBasket } from 'src/app/shared/models/basket';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  basket$: Observable<IBasket>;
 
-  constructor() { }
+  constructor(private basketService: BasketService) {}
 
   ngOnInit(): void {
+    this.basket$ = this.basketService.basket$;
   }
-
 }
