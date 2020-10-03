74d69ccd0c84b9cb21e4dac2177537c10108fcdd
isidoreduc <isidoreducasse79@gmail.com>
2020-10-03 14:58:40 +0200

    implementing the payments controller

---
+++ API/Controllers/PaymentsController.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class PaymentsController : BaseApiController
+    {
+        private readonly IPaymentService _paymentService;
+        public PaymentsController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        // we require auth at method level, not class, because Stripe has to access the controller and well we cannot ask Stripe to authenticate
+        [Authorize]
+        [HttpPost("{basketId}")]
+        public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            return await _paymentService.CreateOrUpdatePaymentIntent(basketId);
+        }
+    }
+}
